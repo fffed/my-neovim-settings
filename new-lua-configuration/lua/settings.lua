2e303f9a421dcdda87ec04b70097ca42ddf075c2
fffed <47778360+fffed@users.noreply.github.com>
2021-04-19 19:25:39 +0300

    add settings

--- new-lua-configuration/lua/settings.lua
+++ new-lua-configuration/lua/settings.lua
@@ -3,7 +3,7 @@ local wo = vim.wo -- window-local options
 local bo = vim.bo -- buffer-local options
 
 
--- -- <TAB>
+-- -- <TAB> 
 bo.tabstop     = 4                        --number of spaces that a <Tab> in the file counts for
 bo.shiftwidth  = 4                        --number of spaces to use for each step of (auto)indent
 bo.softtabstop = 4                        --number of spaces that a <Tab> counts for while performing editing operations
@@ -18,10 +18,17 @@ o.diffopt      = o.diffopt .. ',vertical' --Diff vertically instead of horizonta
 o.title        = true                     --turn on current file name for window title
 o.hidden       = true                     --make possible to switch buffers without saving
 o.scrolloff    = 5                        --add scroll offset while scrolling
+o.ignorecase   = true                     --ignore case in search patterns
+o.smartcase    = true                     --override the 'ignorecase' option if the search pattern contains upper case characters
+o.listchars   = 'tab:▸·,trail:·'          --to override default from "tab:> ,trail:-,nbsp:+"
+o.clipboard    = 'unnamedplus'            --To ALWAYS use the clipboard for ALL operations (instead of interacting with the '+' and/or '*' registers explicitly)
 
 wo.number      = true                     --print the line number in front of each line
 wo.wrap        = false                    --display long lines as one line
 wo.cursorline  = true                     --highlighting of the current line
+wo.list        = true                     --show special characters for tabs, spaces, etc
+
 
 bo.spelllang   = 'en_us,ru_yo'            --set languages for spell checking
 bo.smartindent = true                     --do smart autoindenting when starting a new line
+bo.undofile    = true                     --automatically saves undo history to a file when writing a buffer to a file, and restores undo history from the same file on buffer read
